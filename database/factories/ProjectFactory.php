7bbc544ae9753b33846c5bafab86d3ad8dc28b40
Ivan Lazic <ivanzlazic@gmail.com>
2023-01-03 19:24:18 +0100

    section 2 completed. install jetstrea, create models, and test  data

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Project;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $colorName = $this->faker->randomElement(Project::getAvailableTextColors());
+        $iconName = $this->faker->randomElement(Project::getAvailableIcons());
+
+        return [
+            'title' => $this->faker->unique()->word(2, true),
+            'description' => $this->faker->sentence(),
+            'color' => $colorName,
+            'icon_name' => $iconName,
+        ];
+    }
+}
---
+++ database/factories/SkillFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Skill;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Skill>
+ */
+class SkillFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+        $color = $this->faker->randomElement(Skill::getAvailableBackgroundColors());
+
+        return [
+            'name' => $this->faker->unique()->word(),
+            'color' => $color
+        ];
+    }
+}
